dddf64469e68ae593d3fc77716a13605171c228b
wxx123 <stellarwxx@163.com>
2022-01-23 17:44:56 +0800

    netpoll: update demo

--- netpoll/basic/client/client.go
+++ netpoll/basic/client/client.go
@@ -14,16 +14,17 @@ func main() {
 		panic("dial netpoll connection failed")
 	}
 	reader, writer := conn.Reader(), conn.Writer()
-	// reading
-	buf, _ := reader.Next(0)
-	// parse the read data
-	fmt.Println(buf)
-	reader.Release()
 
 	// writing
-	var write_data = []byte("hello")
+	var write_data = []byte("hello from client") 
 	// make the write data
 	alloc, _ := writer.Malloc(len(write_data))
 	copy(alloc, write_data) // write data
 	writer.Flush()
+
+	// reading
+	buf, _ := reader.Next(17)
+	// parse the read data
+	fmt.Println("data from server: " + string(buf))
+	reader.Release()
 }
--- netpoll/basic/server/main.go
+++ netpoll/basic/server/main.go
@@ -22,13 +22,13 @@ func main() {
 func handler (ctx context.Context, connection netpoll.Connection) error {
 	reader, writer := connection.Reader(), connection.Writer()
 	// reading
-	buf, _ := reader.Next(1)
+	buf, _ := reader.Next(17)
 	// parse the read data
 	fmt.Println("data from client: ", string(buf))
 	reader.Release()
 
 	// writing
-	var write_data []byte
+	var write_data = []byte("hello from server")
 	// make the write data
 	alloc, _ := writer.Malloc(len(write_data))
 	copy(alloc, write_data) // write data
